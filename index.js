b7ff484955a95905d96712540e351815c20307d6
Valera <v-chupurnov@edadeal.ru>
2019-02-19 17:41:59 +0300

    First variant

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+/**
+ * main stylint kickoff function
+ *
+ * @param {string} path   [custom path if used programmatically]
+ * @param {object} config [config if used programmatically]
+ * @return {Object} [the composed stylint object]
+ */
+var StylusLinter = function(path, config, callback) {
+	var Lint = function() {};
+
+
+
+	return Lint
+};
+
+module.exports = StylusLinter
