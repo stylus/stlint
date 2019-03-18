ff5daec83993e0290835f0500c14d8c5cf6c5336
Valera <v-chupurnov@edadeal.ru>
2019-03-18 17:43:33 +0300

    Added parsing hash in context.vars

--- tests/beforeCheckNodeTest.ts
+++ tests/beforeCheckNodeTest.ts
@@ -7,17 +7,18 @@ describe('Before check node checker doing some work', () => {
 		const linter = new Linter();
 
 		linter.lint('./test.styl', `$p = {
-			colors: #ccc #ddd #eee rgba(0, 0, 0, 0.1)
-			width: basis(10, 20px, #ccc) 1px solid,
-			color: {
-				fff: black red
-			},
-			background: $colors.white[0],
-			offersShopsText: $colors.grey[2],
-			discountBgColor: $colors.base.third
-		}
-		
-		color = #ccc`);
+				colors: #ccc #ddd #eee rgba(0, 0, 0, 0.1)
+				width: basis(10, 20px, #ccc) 1px solid,
+				color: {
+					fff: black red
+				},
+				background: $colors.white[0],
+				offersShopsText: $colors.grey[2],
+				discountBgColor: $colors.base.third
+			}
+			
+			color = #ccc`
+		);
 
 		expect(Rule.getContext().vars).to.be.deep.equal({
 			"$p": {
