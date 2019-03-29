f6a7833149df25b87fbd4d2dfd591865bc8edd6f
Valera <v-chupurnov@edadeal.ru>
2019-03-29 13:46:39 +0300

    Фикс документатор баг

--- src/doc.ts
+++ src/doc.ts
@@ -1,6 +1,5 @@
-import { Documentator } from './core/documentator/documentator';
-
 export const doc = (options = {}) => {
+	const { Documentator } = require('./core/documentator/documentator');
 	const documentator = new Documentator(options);
 	documentator.generate();
 };
